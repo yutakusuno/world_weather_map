9b4d37ed270f78a68a4c9f6f545ad784f1cb97d8
Yuta Kusuno <yutakusuno51@gmail.com>
2023-02-12 19:15:19 +0900

    Get weather data using Open-Meteo

--- frontend/src/components/WebMapView.tsx
+++ frontend/src/components/WebMapView.tsx
@@ -1,20 +1,67 @@
-import React, { useEffect } from "react";
-import { loadModules } from "esri-loader";
+import React, { useEffect, useState } from "react";
+import { loadModules } from "esri-loader"; // https://github.com/Esri/esri-loader
 import "./WebMapView.css";
+import axios from "axios";
 
-// esri-loader https://github.com/Esri/esri-loader
-export const WebMapView = () => {
+type Point = {
+  latitude: number;
+  longitude: number;
+};
+
+export const WebMapView: React.FC = () => {
+  const [data, setData] = useState<any>(null);
+  const [date, setDate] = useState<Date>(new Date());
+  const [point, setPoint] = useState<Point>({ latitude: 0, longitude: 0 });
+  const [ongoing, setOngoing] = useState<Boolean>(false);
+  const [searching, setSearching] = useState<Boolean>(false);
+
   useEffect(() => {
-    const options = { css: true };
-    // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(["esri/views/MapView", "esri/WebMap"], options).then(
+    const checkMultipleClick = () => {
+      const now = new Date();
+      const diff = now.getTime() - date.getTime();
+      setDate(now);
+      return diff < 1000 ? true : false;
+    };
+
+    const searchWeather = async (point: Point) => {
+      if (checkMultipleClick()) return;
+      if (ongoing || !searching) return;
+      setOngoing(true);
+
+      try {
+        const response = await axios.get(
+          `https://api.open-meteo.com/v1/forecast?latitude=${point.latitude}&longitude=${point.longitude}&hourly=temperature_2m`
+        );
+        const resData = response?.data;
+        setData(resData);
+      } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+          console.error("AxiosError", error);
+        } else {
+          console.error("UnexpectedError", error);
+        }
+        setOngoing(false);
+        setSearching(false);
+      } finally {
+        setOngoing(false);
+        setSearching(false);
+      }
+    };
+    searchWeather({ latitude: point.latitude, longitude: point.longitude });
+  }, [searching]);
+
+  useEffect(() => {
+    console.log("data", data);
+  }, [data]);
+
+  useEffect(() => {
+    loadModules(["esri/views/MapView", "esri/WebMap"], { css: true }).then(
       ([MapView, WebMap]) => {
-        // basemap values https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap
         const webMap = new WebMap({
           basemap: "topo-vector",
         });
 
-        // load the map view at the map DOM node
         const mapView = new MapView({
           container: "map",
           map: webMap,
@@ -22,12 +69,18 @@ export const WebMapView = () => {
           zoom: 3,
         });
 
+        mapView.on("click", (event: any) => {
+          const point = mapView.toMap({ x: event.x, y: event.y });
+          setPoint({ latitude: point.latitude, longitude: point.longitude });
+          setSearching(true);
+        });
+
         return () => {
-          if (mapView) mapView.destroy(); // destroy the map view
+          if (mapView) mapView.destroy();
         };
       }
     );
-  });
+  }, []);
 
   return <div id="map" />;
 };
